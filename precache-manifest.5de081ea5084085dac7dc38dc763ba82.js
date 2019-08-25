f22c244945a791084cfc998d3a383271e8dcaa38
Noble Varghese <varghese.noble96@gmail.com>
2019-08-25 20:25:43 +0530

    Updates

---
+++ precache-manifest.5de081ea5084085dac7dc38dc763ba82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "457495ce72353051128e1bb474a8c213",
+    "url": "/My-portfolio_site/static/media/sitebg2.457495ce.jpg"
+  },
+  {
+    "revision": "0852ab0015d43f2e6540bf0fc82864ff",
+    "url": "/My-portfolio_site/static/media/sitebg.0852ab00.jpg"
+  },
+  {
+    "revision": "cba0b89d2bf2d96a1ed26edb5849f804",
+    "url": "/My-portfolio_site/static/media/react-logo.cba0b89d.png"
+  },
+  {
+    "revision": "e0d3ac7b7bfd590a87619fc5bc0396f1",
+    "url": "/My-portfolio_site/static/media/buildings.e0d3ac7b.jpg"
+  },
+  {
+    "revision": "40a3cf9cb2ea1c566434",
+    "url": "/My-portfolio_site/static/js/runtime~main.40a3cf9c.js"
+  },
+  {
+    "revision": "4a96ddba956cd2959c14",
+    "url": "/My-portfolio_site/static/js/main.4a96ddba.chunk.js"
+  },
+  {
+    "revision": "d8276796bffa66cd0b0a",
+    "url": "/My-portfolio_site/static/js/1.d8276796.chunk.js"
+  },
+  {
+    "revision": "4a96ddba956cd2959c14",
+    "url": "/My-portfolio_site/static/css/main.4f8b4896.chunk.css"
+  },
+  {
+    "revision": "d8276796bffa66cd0b0a",
+    "url": "/My-portfolio_site/static/css/1.e4bd71d0.chunk.css"
+  },
+  {
+    "revision": "56701da50865ed4b2dc1df55460cb5b5",
+    "url": "/My-portfolio_site/index.html"
+  }
+];
